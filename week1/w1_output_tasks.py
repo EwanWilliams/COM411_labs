953a2efc0ee96428bf177657ac5fa4801bbe6ad1
EwanWilliams <ewan@ewan.name>
2023-09-28 12:20:47 +0100

    Output tasks complete

---
+++ week1/w1_output_tasks.py
@@ -0,0 +1,17 @@
+def main():
+    #wow a comment, print function next
+    print("Welcome to COM411!")
+    print("In week 1 we will learn...\n")
+    print("...How to use Git and GitHub")
+    print("...How to output to the screen")
+    print("...How to get user input\n")
+    print("I hope you are enjoying the lesson thus far!")
+
+    print("##########")
+    print("#  -   - #")
+    print("#   ___  #")
+    print("##########")
+
+
+if __name__ == "__main__":
+    main()

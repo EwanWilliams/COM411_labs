aeb8d3ba3f1a03d445e7c2a9043c57944a5607f7
EwanWilliams <ewan@ewan.name>
2023-11-17 10:17:09 +0000

    Week 7 json tasks completed

--- week7/w7_json_tasks.py
+++ week7/w7_json_tasks.py
@@ -2,6 +2,7 @@ import json
 
 def main():
     run()
+    run_task2()
 
 
 #task 1
@@ -18,6 +19,24 @@ def read(file_path):
     for bot in data["bots"]:
         print(f"{bot['name']} has the following stats: {bot['stats']}")
 
+
+#task 2
+def run_task2():
+    json_data = read_task2("futurama.json")
+    save("exported.json", json_data)
+
+def read_task2(file_path):
+    print("Reading...")
+    with open(file_path) as f:
+        data = json.load(f)
+    print("Done!")
+    return data
+
+def save(file_path, data):
+    print("Exporting...")
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent = 4)
+    print("Done!")
 
 
 if __name__ == "__main__":
